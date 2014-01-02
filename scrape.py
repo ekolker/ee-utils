0a3a850b97ca05bbc986f9b9b05eb9ec6de0d4c4
ekolker <e.kolker@gmail.com>
2014-01-02 13:49:55 -0500

    multiple part numbers, flags invalid ones

--- scrape.py
+++ scrape.py
@@ -1,29 +1,49 @@
 from bs4 import BeautifulSoup
 import requests, sys
+from types import NoneType
 
-def main(name):
-  r = requests.get('http://www.digikey.com/product-detail/en/CBMF1608T470K/587-1722-1-ND/1008047')
-   
-  page_source = r.text.encode("utf8")
+def main(name, *args):
 
-  # cut to the chase
-  target = '<table class="product-details-table" '
-  start_index = page_source.find(target)
-  ingredients = page_source[start_index : len(page_source) - 26]
+  if args == ():
+    args = ['ACML-0603-121-TCT-ND']
+  print args
 
-  soup = BeautifulSoup(ingredients)
+  base_url = 'http://search.digikey.com/scripts/DkSearch/dksus.dll?Detail&name='
 
-  Manufacturer = soup.find(itemprop="manufacturer").find(itemprop="name").contents[0]
-  DigiKeyPN = soup.find(id="reportpartnumber").contents[1]
-  ManufacturerPN = soup.find(class_="seohtag", itemprop='model').contents[0]
-  Description = soup.find(itemprop="description").contents[0]
-  Datasheets = []
-  for link in soup.find_all(class_="lnkDatasheet"):
-    # there could be more than one...
-    Datasheets.append(link.get('href'))
+  invalids = []
 
-  print [Manufacturer, ManufacturerPN, DigiKeyPN, Description, Datasheets], '\n'
+  for argument_number in range(len(args)):
+    r = requests.get(base_url + args[argument_number])
+     
+    page_source = r.text.encode("utf8")
 
+    # cut to the chase
+    target = '<table class="product-details-table" '
+    start_index = page_source.find(target)
+    ingredients = page_source[start_index : len(page_source) - 26]
+
+    soup = BeautifulSoup(ingredients)
+
+    if type(soup.find(itemprop="manufacturer")) == NoneType:
+      # skip invalid part numbers
+      invalids.append(args[argument_number])
+      continue
+
+    Manufacturer = soup.find(itemprop="manufacturer").find(itemprop="name").contents[0]
+    DigiKeyPN = soup.find(id="reportpartnumber").contents[1]
+    ManufacturerPN = soup.find(class_="seohtag", itemprop='model').contents[0]
+    Description = soup.find(itemprop="description").contents[0]
+    Datasheets = []
+    for link in soup.find_all(class_="lnkDatasheet"):
+      # there could be more than one...
+      Datasheets.append(link.get('href'))
+
+    print [Manufacturer, ManufacturerPN, DigiKeyPN, Description, Datasheets], '\n'
+
+  if (len(invalids) > 0):
+    print "Invalid part numbers:"
+    for i in invalids:
+      print i
 
   # print details
 
